2cf9286bfa74ed835096b8091c7dc140cbb2dd56
Gordon MacMaster <gmacmast@uvm.edu>
2019-07-16 22:12:19 -0700

    Updates

---
+++ precache-manifest.06ee43caac9d0acc58acc9f0cef0a93d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94e9470d6de31e964caa959af1cb42d7",
+    "url": "/static/media/me.94e9470d.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "507cda1896c070ef1b3f",
+    "url": "/static/js/main.507cda18.chunk.js"
+  },
+  {
+    "revision": "9b5e2b78e05b92410f45",
+    "url": "/static/js/2.9b5e2b78.chunk.js"
+  },
+  {
+    "revision": "507cda1896c070ef1b3f",
+    "url": "/static/css/main.4f0fcdb0.chunk.css"
+  },
+  {
+    "revision": "9b5e2b78e05b92410f45",
+    "url": "/static/css/2.743cdb84.chunk.css"
+  },
+  {
+    "revision": "22b3d066daeb8efc47a0f8c56c59de7b",
+    "url": "/index.html"
+  }
+];
