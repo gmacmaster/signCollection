c15fbb1ed42339aee18398951875ede14a85a553
Gordon MacMaster <gmacmast@uvm.edu>
2019-07-16 22:37:01 -0700

    add docs folder

---
+++ docs/precache-manifest.3c3fcb528c7675e6bace3785cf6143da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94e9470d6de31e964caa959af1cb42d7",
+    "url": "/static/media/me.94e9470d.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "9f4207e0c65834785652",
+    "url": "/static/js/main.9f4207e0.chunk.js"
+  },
+  {
+    "revision": "9b5e2b78e05b92410f45",
+    "url": "/static/js/2.9b5e2b78.chunk.js"
+  },
+  {
+    "revision": "9f4207e0c65834785652",
+    "url": "/static/css/main.4f0fcdb0.chunk.css"
+  },
+  {
+    "revision": "9b5e2b78e05b92410f45",
+    "url": "/static/css/2.743cdb84.chunk.css"
+  },
+  {
+    "revision": "5f7014b5e0a7e969cf18ee91061efa0e",
+    "url": "/index.html"
+  }
+];
